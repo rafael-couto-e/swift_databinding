ed04d36aef26ebe31b15659854b031dad6624ef8
Rafael Couto Estrela <rca.rafaelcouto@MacBook-Pro-de-GS.local>
2019-10-04 13:39:56 -0300

    Added scrollview

--- RCADataBinding/ViewController/MainViewController.swift
+++ RCADataBinding/ViewController/MainViewController.swift
@@ -23,6 +23,7 @@ class MainViewController: UIViewController {
     private let usernameField: UITextField = UITextField(frame: .zero)
     private let passwordField: UITextField = UITextField(frame: .zero)
     private let confirmButton: UIButton = UIButton(frame: .zero)
+    private let scrollView: UIScrollView = UIScrollView(frame: .zero)
     
     // MARK: properties
     
@@ -69,11 +70,13 @@ class MainViewController: UIViewController {
     }
     
     private func setupLayout() {
-        self.view.addSubviews([
+        self.scrollView.addSubviews([
             self.usernameField,
             self.passwordField,
             self.confirmButton
         ])
+        
+        self.view.addSubviews([scrollView])
         
         self.usernameField.borderStyle = .roundedRect
         self.usernameField.placeholder = "Login"
@@ -92,19 +95,28 @@ class MainViewController: UIViewController {
     }
     
     private func setupConstraints() {
-        self.usernameField.top(to: self.view.safeAreaLayoutGuide.topAnchor, padding: 16)
-        self.usernameField.leading(to: self.view.leadingAnchor, padding: 16)
-        self.usernameField.trailing(to: self.view.trailingAnchor, padding: 16)
+        self.scrollView.top(to: self.view.safeAreaLayoutGuide.topAnchor, padding: 0)
+        self.scrollView.leading(to: self.view.leadingAnchor, padding: 0)
+        self.scrollView.trailing(to: self.view.trailingAnchor, padding: 0)
+        self.scrollView.bottom(to: self.view.bottomAnchor, padding: 0)
+        
+        self.usernameField.centerX(to: scrollView)
+        
+        self.usernameField.top(to: self.scrollView.topAnchor, padding: 16)
+        self.usernameField.leading(to: self.scrollView.leadingAnchor, padding: 16)
+        self.usernameField.trailing(to: self.scrollView.trailingAnchor, padding: 16)
         self.usernameField.heigth(equalTo: 40)
         
         self.passwordField.top(to: self.usernameField.bottomAnchor, padding: 16)
-        self.passwordField.leading(to: self.view.leadingAnchor, padding: 16)
-        self.passwordField.trailing(to: self.view.trailingAnchor, padding: 16)
+        self.passwordField.leading(to: self.scrollView.leadingAnchor, padding: 16)
+        self.passwordField.trailing(to: self.scrollView.trailingAnchor, padding: 16)
         self.passwordField.heigth(equalTo: 40)
         
         self.confirmButton.top(to: self.passwordField.bottomAnchor, padding: 16)
-        self.confirmButton.leading(to: self.view.leadingAnchor, padding: 16)
-        self.confirmButton.trailing(to: self.view.trailingAnchor, padding: 16)
+        self.confirmButton.leading(to: self.scrollView.leadingAnchor, padding: 16)
+        self.confirmButton.trailing(to: self.scrollView.trailingAnchor, padding: 16)
+        self.confirmButton.bottom(to: self.scrollView.bottomAnchor, padding: 16)
+        
         self.confirmButton.heigth(equalTo: 40)
     }
     
